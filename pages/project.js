a74ac7dad5569df73fbb626804c3b3b6f6771a44
Al Madireddy <al.madireddy@gmail.com>
2019-03-01 12:10:02 -0600

    added involved members to projects page

--- pages/project.js
+++ pages/project.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "reactstrap";
 import Layout from "../components/Layout";
 import fetch from "node-fetch";
 import BreadcrumbRow from "../components/Breadcrumb";
+import DynamicLink from "../components/DynamicLink";
 
 export default class Project extends React.Component {
   constructor(props) {
@@ -13,7 +14,6 @@ export default class Project extends React.Component {
   static async getInitialProps({ query }) {
     const res = await fetch(`https://dev.atec.io/wp-json/wp/v2/project/${query.id}`)
     let data = await res.json()
-    console.log(data)
     return { project: data }
   }
 
@@ -26,9 +26,24 @@ export default class Project extends React.Component {
       <Layout pageName={this.props.project.title.rendered} title={this.props.project.title.rendered}>
         <Container>
           <BreadcrumbRow parentHref='/projects' parentText="Projects" activeText={this.props.project.title.rendered}></BreadcrumbRow>
-          <Row>
+          <Row className='mb-5'>
             <Col dangerouslySetInnerHTML={this.renderContent()}>
             </Col>
+          </Row>
+          <Row className='mb-3'>
+            <Col>
+              <h2>People involved</h2>
+            </Col>
+          </Row>
+          <Row>
+            {this.props.project.lab_members.map((person) => (
+              <Col sm='3' className='mb-5'>
+                <DynamicLink actualRoute='person' displayRoute='people' id={person.id}>
+                  <img className='img-fluid' src={person.profile_image.guid}></img>
+                  <h2>{person.post_title}</h2>
+                </DynamicLink>
+              </Col>
+            ))}
           </Row>
         </Container>
       </Layout>
