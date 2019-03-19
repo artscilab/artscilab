90492d608d6955565437a7f6502c663f883f16a2
Al Madireddy <al.madireddy@gmail.com>
2019-03-19 09:39:00 -0500

    small shadow changes

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import DynamicLink from "../components/DynamicLink";
 import SiteNav from '../components/SiteNav';
+import ReactFitText from 'react-fittext';
 
 export default class Index extends Component {
   static async getInitialProps() {
@@ -21,13 +22,14 @@ export default class Index extends Component {
         <Container fluid>
           <Row className='home-header'>
             <Col md='6' className='home-left'>
-              <h1 className='site-title'>ART<br></br>SCI<br></br>LAB</h1>
-              <p className='site-lead'>The ArtSciLab is a transdisciplinary research lab that carries out national and international investigations on <span className='data-vis'>data visualization,</span> <span className='experimental-publishing'>experimental publishing</span> and the <span className='hybridization'>hybridization of art and science</span></p>
+              <ReactFitText compressor={0.3} maxFontSize={150}>
+                <h1 className='site-title'>ART<br></br>SCI<br></br>LAB</h1>
+              </ReactFitText>
             </Col>
             <Col md='6' className='home-right'>
               <div className='intro'>
-                <h2>ART and SCIENCE</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur..</p>
+                <h2>ART/SCIENCE</h2>
+                <p className='site-lead'>The ArtSciLab is a transdisciplinary research lab that carries out national and international investigations on <span className='data-vis'>data visualization,</span> <span className='experimental-publishing'>experimental publishing</span> and the <span className='hybridization'>hybridization of art and science</span></p>
               </div>
               <div className='intro-bg'></div>
             </Col>
@@ -35,38 +37,44 @@ export default class Index extends Component {
         </Container>
         <Container>
           <SiteNav title={this.props.title}></SiteNav>
-          <Row className='mt-5 mb-3'>
-            <Col>
+          <Row className='mt-5 mb-5'>
+            <Col className='text-center'>
               <h2>Recent Projects</h2>
-              <hr></hr>
             </Col>
           </Row>
-          <Row className='mb-3'>
-            {this.props.projects.map((project) => (
-              <Col sm='3' className='listing'>
-                <DynamicLink actualRoute='project' displayRoute='projects' slug={project.slug}>
-                  {project.research_image && <div className='crop'>
-                    <img src={project.research_image.guid}></img>
-                  </div>}
-                  <h2>{project.title.rendered}</h2>
-                </DynamicLink>
-              </Col>
-            ))}
+          <Row className='justify-content-center mb-3'>
+            <Col sm='8'>
+              <Row>
+                {this.props.projects.map((project) => (
+                  <Col sm='6' className='listing'>
+                    <DynamicLink actualRoute='project' displayRoute='projects' slug={project.slug}>
+                      {project.research_image && <div className='crop'>
+                        <img src={project.research_image.guid}></img>
+                      </div>}
+                      <h2>{project.title.rendered}</h2>
+                    </DynamicLink>
+                  </Col>
+                ))}
+              </Row>
+            </Col>
           </Row>
-          <Row className='mt-5 mb-3'>
-            <Col>
+          <Row className='mt-5 mb-5'>
+            <Col className='text-center'>
               <h2>Recent Posts</h2>
-              <hr></hr>
             </Col>
           </Row>
-          <Row className='mb-3'>
-            {this.props.posts.map((post) => (
-              <Col sm='3' className='listing'>
-                <DynamicLink actualRoute='post' displayRoute='posts' slug={post.slug} >
-                  <h2>{post.title.rendered}</h2>
-                </DynamicLink>
-              </Col>
-            ))}
+          <Row className='mb-3 justify-content-center'>
+            <Col sm='8'>
+              <Row>
+                {this.props.posts.map((post) => (
+                  <Col sm='6' className='listing'>
+                    <DynamicLink actualRoute='post' displayRoute='posts' slug={post.slug} >
+                      <h2>{post.title.rendered}</h2>
+                    </DynamicLink>
+                  </Col>
+                ))}
+              </Row>
+            </Col>
           </Row>
         </Container>
       </Layout>
