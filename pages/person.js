7907e0262cceb8ffac69f9e4c4601e3494dceec0
Al Madireddy <al.madireddy@gmail.com>
2019-04-04 19:45:41 -0500

    people single page

--- pages/person.js
+++ pages/person.js
@@ -3,7 +3,8 @@ import fetch from "node-fetch";
 import Layout from "../components/Layout";
 import { Container, Col, Row, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import DynamicLink from "../components/DynamicLink";
-export default class Person extends React.Component {
+
+export default class Person extends Component {
   constructor(props) {
     super(props)
     this.renderPost = this.renderPost.bind(this)
@@ -22,21 +23,22 @@ export default class Person extends React.Component {
   render() {
     return(
       <Layout pageName={this.props.person.title.rendered} title={this.props.person.title.rendered}>
-        <Container>
-          <Row className='mb-5 mt-4 justify-content-center'>
-            <Col md='8 d-flex flex-column align-items-center justify-content-center'>
-              <img className="img-fluid" src={this.props.person.profile_image.guid}></img>
-              <h4 className='mt-4 mb-3'>{this.props.person.member_title}</h4>
-              {!!this.props.person.skills && 
-                <p>{this.props.person.skills}</p>
-              }
-              {!!this.props.person.website && 
-                <p><a href={this.props.person.website}>{this.props.person.website}</a></p>
-              }
+        <Container className='person-single'>
+          <Row className='person-header align-items-end justify-content-center'>
+            <Col md='5'>
+              <img className='img-fluid' src={this.props.person.profile_image.guid}></img>
             </Col>
-          </Row>
-          <Row className='mb-5 justify-content-center'>
-            <Col md='7' dangerouslySetInnerHTML={this.renderPost()}>
+            <Col md='6'>
+              <div className='person-name'>
+                <h1 className='display-3'>{this.props.person.title.rendered}</h1>
+              </div>
+            </Col>
+          </Row> 
+          <Row className='person-info align-items-baseline justify-content-center'>
+            <Col md='4'>
+              <h2 className='person-job-title'>{this.props.person.member_title}</h2>
+            </Col>
+            <Col className='person-bio-container' md='7' dangerouslySetInnerHTML={this.renderPost()}>
             </Col>
           </Row>
           {this.props.person.project_involved.length > 0 &&
