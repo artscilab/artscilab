e75884b3f6426597605e7d188ff3b33f4aaffadb
Al Madireddy <al.madireddy@gmail.com>
2019-10-14 23:27:06 -0500

    home page news and events

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,13 @@ import { Container, Row, Col } from 'reactstrap';
 import DynamicLink from "../components/DynamicLink";
 import SiteNav from '../components/SiteNav';
 import SectionTitle from "../components/SectionTitle";
-import { convertUrlToHttps } from "../functions/functions";
+import { convertUrlToHttps, formatDate } from "../functions/functions";
 
 export default class Index extends Component {
   static async getInitialProps() {
-    const res = await fetch('https://dev.atec.io/wp-json/wp/v2/project?per_page=4')
-    let projects = await res.json()
-
-    const res2 = await fetch('https://dev.atec.io/wp-json/wp/v2/posts?per_page=6')
+    const res2 = await fetch('https://dev.atec.io/wp-json/wp/v2/posts?per_page=6&_embed')
     let posts = await res2.json()
-    return { projects, posts }
+    return { posts }
   }
 
   render() {
@@ -32,33 +29,48 @@ export default class Index extends Component {
         </Container>
 
         <Container>
-          <SectionTitle text="Recent Projects"></SectionTitle>
-          <Row>
-            {this.props.projects.map((project) => (
-              <Col sm='4' className='listing'>
-                <DynamicLink 
-                  actualRoute='project' 
-                  displayRoute='projects' 
-                  slug={project.slug}>
-                    {console.log(project)}
-                    {project.research_image && <div className='crop'>
-                      <img src={convertUrlToHttps(project.research_image.guid)}></img>
-                    </div>}
-                    <h3 dangerouslySetInnerHTML={{__html: project.title.rendered}}></h3>
-                </DynamicLink>
-              </Col>
-            ))}
-          </Row>
-
           <SectionTitle text="News and Events"></SectionTitle>
           <Row className='mb-3 justify-content-center'>
-            {this.props.posts.map((post) => (
-              <Col sm='4' className='listing'>
-                <DynamicLink actualRoute='post' displayRoute='posts' slug={post.slug} >
-                  <h3 dangerouslySetInnerHTML={{__html: post.title.rendered}}></h3>
-                </DynamicLink>
-              </Col>
-            ))}
+            {this.props.posts.map((post) => {
+              let excerpt = "";
+
+              if (post.excerpt.rendered.length > 125) {
+                excerpt = post.excerpt.rendered.substring(0, 125) + ' ...'
+              } else {
+                excerpt = post.excerpt.rendered
+              }
+              
+              return (
+                <Col sm='4' className='listing home-listing'>
+                  <div>
+                    {post._embedded &&
+                      post._embedded['wp:featuredmedia'] && 
+                      post._embedded['wp:featuredmedia'][0] && 
+                      post._embedded['wp:featuredmedia'][0].media_details && 
+                      post._embedded['wp:featuredmedia'][0].media_details.sizes && 
+                      post._embedded['wp:featuredmedia'][0].media_details.sizes.medium && 
+                      <div className='crop'>
+                        <img src={convertUrlToHttps(post._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url)}></img>
+                      </div>
+                    }
+  
+                    <h3 dangerouslySetInnerHTML={{__html: post.title.rendered}}></h3>
+                  
+                    {post.excerpt &&
+                      <div className="excerpt" dangerouslySetInnerHTML={{__html: excerpt}}></div>
+                    }
+                    
+                    <DynamicLink actualRoute='post' displayRoute='posts' slug={post.slug} >
+                      <button>Read More ></button>
+                    </DynamicLink>
+                    
+                    <div className="date">
+                      <p>{formatDate(new Date(post.date))}</p>
+                    </div>
+                  </div>
+                </Col>
+              )
+            })}
           </Row>
         </Container>
       </Layout>
