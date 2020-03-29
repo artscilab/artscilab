b9358005a0c3f1f87b0ef6e2040084b577f70f9d
Al Madireddy <al.madireddy@gmail.com>
2020-03-29 16:03:25 -0500

    clustering isnt supported atm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     name: 'artsci',
     script: 'npm',
     args: 'start',
-    instances: "max",
-    exec_mode: "cluster",
+    instances: 1,
+    watch: true,
     env: {
       NODE_ENV: 'development'
     },
