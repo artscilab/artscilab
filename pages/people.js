86215e66e00f9ca965fe5268a3f7b0b2ace00070
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2021-07-01 11:23:25 -0500

    Update lab member and individual person pages

--- pages/people.js
+++ pages/people.js
@@ -9,21 +9,22 @@ export default class People extends Component {
   static async getInitialProps() {
     const res2 = await fetch('https://dev.atec.io/wp-json/wp/v2/lab_member?per_page=99&orderby=title&order=asc')
     let members = await res2.json()
+    console.log(members[0]);
     return { members }
   }
   
   render() {
     return (
-      <Layout title="People" pageName="People">
+      <Layout title="Lab Members" pageName="Lab Members">
         <Container>
           <Row className='justify-content-center'>
             {this.props.members.map((person) => (
-              <Col md='3' className='mb-5 listing' >
+              <Col xs='12' sm='6' md='4' className='mb-5 listing' key={person.id}>
                 <DynamicLink actualRoute='person' displayRoute='people' slug={person.slug}>
                   <div className='crop'>
                     <img className='img-fluid' src={convertUrlToHttps(person.profile_image.guid)}></img>
                   </div>
-                  <h2>{person.title.rendered}</h2>
+                  <h4>{person.title.rendered}</h4>
                   {person.member_title && <p>{person.member_title}</p> }
                 </DynamicLink>
               </Col>
--- pages/person.js
+++ pages/person.js
@@ -26,43 +26,65 @@ export default class Person extends Component {
     return(
       <Layout title={this.props.person.title.rendered}>
         <Container className='single-item'>
+
           <Row className='single-header align-items-end justify-content-center'>
-            <Col md='5' className='single-feature-img'>
+
+            <Col xs='12' sm='12' md='5' className='single-feature-img'>
               <img className='img-fluid' src={convertUrlToHttps(this.props.person.profile_image.guid)}></img>
             </Col>
-            <Col md='7'>
-              <div className='single-title'>
-                <ReactFitText compressor={0.6} maxFontSize={75}>
-                  <h1 className='display-3'>{this.props.person.title.rendered}</h1>
+
+            <Col xs='12' sm='12' md='7'>
+              <div>
+                <ReactFitText compressor={0.6} maxFontSize={50}>
+                  <h3 className='display-3'>{this.props.person.title.rendered}</h3>
                 </ReactFitText>
               </div>
             </Col>
-          </Row> 
-          <Row className='single-information align-items-baseline justify-content-center'>
-            <Col md='4'>
-              <div className='person-job-title'>
-                <ReactFitText compressor={0.9} maxFontSize={55}>
-                  <h2>{this.props.person.member_title}</h2>
-                </ReactFitText>
-                {this.props.person.website && 
-                <a target='blank' href={this.props.person.website}>{this.props.person.website}</a>}
+
+          </Row>
+
+          <Row className='pt-5'>
+            <Col xs='12'>
+              <div>
+                {/* <ReactFitText compressor={0.9} maxFontSize={20}> */}
+                  <h4 className='display-5'>{this.props.person.member_title}</h4>
+                {/* </ReactFitText> */}
               </div>
-            </Col>
-            <Col className='person-bio-container' md='7' dangerouslySetInnerHTML={this.renderPost()}>
             </Col>
           </Row>
+
+          <Row>
+            <Col xs='12'>
+              <div>
+                  <h5 className='display-5'>{
+                    this.props.person.website && 
+                    <a target='blank' href={this.props.person.website}>{this.props.person.website}</a>
+                  }</h5>
+              </div>
+            </Col>
+          </Row>
+
+          <Row className='pt-4 align-items-baseline justify-content-center'>
+            <Col className='person-bio-container' xs='12' dangerouslySetInnerHTML={this.renderPost()}>
+            </Col>
+          </Row>
+
+          <h4 className='pb-4'>Current Projects</h4>
+
           {this.props.person.project_involved.length > 0 &&
-            <Row className='person-projects justify-content-center'>
+            <Row className='justify-content-center'>
             {this.props.person.project_involved.map((project, i) => (
-              <Col sm='3' className='listing project-listing text-center'>
+              <Col xs='6' sm='6' md='4' lg='2' className='listing project-listing text-center'>
                 <DynamicLink actualRoute='project' displayRoute='projects' slug={project.post_name}>
-                  <img className='img-fluid' src={convertUrlToHttps(project.research_image.guid)}></img>
-                  <h2>{project.post_title}</h2>
+                  <img className='img-thumbnail border' src={convertUrlToHttps(project.research_image.guid)}></img>
+                  <h6>{project.post_title}</h6>
                 </DynamicLink>
               </Col>
             ))}
             </Row>
           }
+
+
         </Container>
       </Layout>
     )
