e61e2bed206987911c2d09da8f9f9af8bc9199bb
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2022-01-24 08:31:44 -0600

    Add new Watering Hole announcement

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default class Index extends Component {
                 </a>
               </button>
               <img
-                src='/static/WateringHoleNew.png'
+                src='/static/WateringHole.png'
                 alt='Watering Hole Announcement'
                 className='my-2 border rounded'
               />
