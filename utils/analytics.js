3fcf0808989765d70a9633799d0eabe431416ff5
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2022-02-28 12:52:49 -0600

    Adding google analytics

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,25 +1,25 @@
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  ReactGA.initialize("UA-162354631-1");
-}
+  ReactGA.initialize('G-4VZS72ZLJT');
+};
 
 export const logPageView = () => {
   const { pathname } = window.location;
   ReactGA.set({
-    page: pathname
-  })
+    page: pathname,
+  });
   ReactGA.pageview(pathname);
-}
+};
 
 export const logEvent = (category = '', action = '') => {
   if (category && action) {
-    ReactGA.event({ category, action })
+    ReactGA.event({ category, action });
   }
-}
+};
 
 export const logException = (description = '', fatal = false) => {
   if (description) {
-    ReactGA.exception({ description, fatal })
+    ReactGA.exception({ description, fatal });
   }
-}
+};
