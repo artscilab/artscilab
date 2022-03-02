859b1807054b43c5b439bae3f66172a6f9140697
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2022-03-02 12:37:50 -0600

    Update projects design

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,15 @@
 import Layout from '../components/Layout';
 import fetch from 'node-fetch';
 import { Component } from 'react';
-import { Container, Row, Col, Card, CardBody, CardTitle } from 'reactstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardBody,
+  CardTitle,
+  CardFooter,
+} from 'reactstrap';
 import DynamicLink from '../components/DynamicLink';
 import { convertUrlToHttps } from '../functions/functions';
 import CardText from 'reactstrap/lib/CardText';
@@ -20,36 +28,50 @@ export default class Projects extends Component {
       <Layout title='Projects' pageName='Projects'>
         <Container>
           <Row className='justify-content-center mb-5'>
-            {this.props.projects.map((project, i) =>
-              project.start_year.substring(0, 4) === '2022' ? (
+            {this.props.projects.map((project, i) => {
+              let short_description = '';
+
+              if (project.short_description.length > 100) {
+                short_description =
+                  project.short_description.substring(0, 100) + ' ...';
+              } else {
+                short_description = project.short_description;
+              }
+
+              return project.start_year.substring(0, 4) === '2022' ? (
                 <Col
                   xs='12'
                   sm='6'
                   md='4'
-                  lg='3'
+                  lg='4'
                   key={project.slug}
                   className='mb-5 listing text-center'>
-                  <div>
-                    <DynamicLink
-                      actualRoute='project'
-                      displayRoute='projects'
-                      slug={project.slug}>
-                      <div>
-                        <img
-                          className='img-fluid'
-                          style={{ borderRadius: 20 + 'px' }}
-                          src={convertUrlToHttps(
-                            project.research_image.guid,
-                          )}></img>
-                      </div>
-                      <h4>{project.title.rendered}</h4>
-                    </DynamicLink>
-                  </div>
+                  <Card className='h-100'>
+                    <CardBody>
+                      <CardTitle tag='h5'>
+                        <DynamicLink
+                          actualRoute='project'
+                          displayRoute='projects'
+                          slug={project.slug}>
+                          <h4 className='card-title'>
+                            {project.title.rendered}
+                          </h4>
+                          <img
+                            className='img-fluid'
+                            style={{ borderRadius: 10 + 'px' }}
+                            src={convertUrlToHttps(
+                              project.research_image.guid,
+                            )}></img>
+                        </DynamicLink>
+                      </CardTitle>
+                      <CardFooter>{short_description}</CardFooter>
+                    </CardBody>
+                  </Card>
                 </Col>
               ) : (
                 <></>
-              ),
-            )}
+              );
+            })}
           </Row>
           <hr />
           <h2 className='p-4'> Past Projects </h2>
